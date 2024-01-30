9682d96a932d621ea38435426d4c227d018c8d85
Ayush <ayushoff2020@gmail.com>
2024-01-30 19:21:50 +0530

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const connect = require("./config/database");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, async () => {
   console.log("====================================");
   console.log("Server Started at port ", PORT);
